4d86c0cf68d3f6f469092bc8335a55ec75ff8cf3
nishavgpal <nishavgpal@gmail.com>
2021-01-11 22:54:44 +0530

    Login Assertion

---
+++ Authentication Kata/Authentication_Kata.py
@@ -0,0 +1,90 @@
+import re
+class Initialization:
+    def __init__(self):
+        self.credentials = {}
+
+
+
+class Login:
+    def check(self, user, pas):
+
+        i.credentials["test"]="test12"
+        if user in i.credentials.keys() and pas == i.credentials[user]:
+            return True
+        else:
+            print('Wrong Username or Password')
+            return False
+
+
+class Register:
+
+    def stringvalidation(self, name):
+
+        if name.isalpha():
+            return True
+        print("Username should have only letters")
+        return False
+
+
+
+    def pwdvalidation(self, password):
+        flag = True
+        if (len(password) <= 6):
+            flag = False
+        elif not (re.search("[a-z]", password) or re.search("[A-Z]", password)):
+            flag = False
+
+        elif not re.search("[0-9]", password):
+            flag = False
+        else:
+            flag == True
+
+        return flag
+
+
+    def register (self,name,password):
+
+            i.credentials[name] = password
+
+
+
+i = Initialization()
+s = Login()
+r = Register()
+Stop = False
+user_check = True
+
+"""
+while Stop == False:
+    tasks = (input('What would you like to do? enter [Register], [Login], or [quit]'))
+    # Calling functions with that class object
+    if tasks == 'Register':
+        name = (input('Please enter username'))
+        user_check = r.stringvalidation(name)
+
+        if user_check == True:
+            password = (input('Please enter password'))
+            pwd_check = r.pwdvalidation(password)
+
+            if pwd_check == True:
+                r.register(name,password)
+                print("User account created successfully")
+                user_check == False
+                pwd_check == False
+
+            else:
+                print("Invalid password")
+                user_check == True
+
+
+    if tasks == 'Login':
+        LoginInfoUser = (input('Please enter Username'))
+        LoginInfoPassword = (input('Please enter Password'))
+        login_check=s.check(LoginInfoUser, LoginInfoPassword)
+        if login_check == True:
+            print("Login success!")
+        else:
+            print("Login failed")
+    if tasks == 'quit':
+        print("See you later!")
+        Stop = True"""
---
+++ Authentication Kata/UnitTestAuthenticationValidation.py
@@ -0,0 +1,54 @@
+from Authentication_Kata import Register
+from Authentication_Kata import Login
+import unittest
+
+
+
+class Test_Register_Validations(unittest.TestCase):
+
+
+    def test_validate_for_only_letters_present_pass(self):
+        result = Register.stringvalidation(self,str("test"))
+        self.assertTrue(result == True)
+
+    def test_validate_for_only_letters_present_with_numbers_fail(self):
+        result = Register.stringvalidation(self,"test122")
+        self.assertTrue(result == False)
+
+    def test_validate_for_minimum_length_of_password_pass(self):
+        result = Register.pwdvalidation(self,"testttt")
+        self.assertFalse(result == True)
+
+    def test_validate_for_minimum_length_of_password_fail(self):
+        result = Register.pwdvalidation(self, "test")
+        self.assertTrue(result == False)
+
+    def test_validate_for_minimum_one_alphabet_pass(self):
+        result = Register.pwdvalidation(self,"test12")
+        self.assertFalse(result == True)
+
+    def test_validate_for_minimum_one_alphabet_fail(self):
+        result = Register.pwdvalidation(self,"34512")
+        self.assertTrue(result == False)
+
+    def test_validate_for_minimum_one_digit_pass(self):
+        result = Register.pwdvalidation(self,"test12")
+        self.assertFalse(result == True)
+
+    def test_validate_for_minimum_one_digit_fail(self):
+        result = Register.pwdvalidation(self, "testtt")
+        self.assertTrue(result == False)
+
+
+class Test_Login_Validations(unittest.TestCase):
+    def test_validate_for_username_present_pass(self):
+        result = Login.check(self, "test", "test12")
+        self.assertTrue(result == True)
+
+    def test_validate_for_username_present_fail(self):
+        result = Login.check(self, "test", "test")
+        self.assertFalse(result == True)
+
+
+if __name__ == '__main__':
+    unittest.main()
