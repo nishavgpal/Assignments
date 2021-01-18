4705bc908388c3a6352ac244ac62b6b72ea8e7da
nishavgpal <nishavgpal@gmail.com>
2021-01-18 22:33:44 +0530

    1st test case pass

---
+++ Bowling Kata/Score.py
@@ -0,0 +1,4 @@
+
+class Score:
+    def Total_score(self):
+        return 100
--- Bowling Kata/ScoreTest.py
+++ Bowling Kata/ScoreTest.py
@@ -1,9 +1,10 @@
 import unittest
+from Score import Score
 
 
 class MyTestCase(unittest.TestCase):
     def test_total_score_of_a_frame(self):
-        self.assertTrue(100, Score.total(10,0))
+        self.assertTrue(100, Score.Total_score(self))
 
 
 if __name__ == '__main__':
